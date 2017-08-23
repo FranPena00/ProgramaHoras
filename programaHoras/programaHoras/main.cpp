a84a974d96276f7726141c863eeee612e2abc64d
misael rivera <jrivera16@ucol.mx>
2017-08-22 23:01:28 -0500

    agregue el codigo

---
+++ programaHoras/programaHoras/main.cpp
@@ -0,0 +1,46 @@
+
+/*Se debe calcular el tiempo trabajado de una persona, mostrando en horas, minutos y segundos, considerando lo siguiente.
+ Se sabe la hora, minutos y segundos de entrada y las horas, minutos y segundos de salida del trabajo de esa persona,
+ Todo ocurre el mismo día, por lo que la hora de salida es igual o mayor a la hora de entrada, considerar el horario a 24 horas.*/
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+    int hora, minuto, segundo, horaF, minutoF, segundoF;
+    int horaTrabajada, minutosTrabajados, segundosTrabajados = 0;
+    
+    cout<<"****Sistema de horario 24 horas****\n";
+    cout << "Ingrese hora, minutos y segundos de entrada: \n";
+    cin>>hora>>minuto>>segundo;
+    hora *= 3600;
+    minuto *=60;
+    
+    int horaInicial = hora + minuto + segundo;
+    //este cout lo puse para ver si se estaba haciendo la conversion bien.
+    cout<<horaInicial;
+    
+    cout << "Ingrese hora, minutos y segundos de salida: \n";
+    cin>>horaF>>minutoF>>segundoF;
+    horaF *=3600;
+    minutoF *=60;
+    int horaFinal = horaF + minutoF + segundoF;
+    //este cout lo puse para ver si se estaba haciendo la conversion bien.
+    cout<<horaFinal;
+    
+        horaTrabajada = horaFinal - horaInicial;
+        //este cout lo puse para ver si se estaba haciendo la conversion bien.
+        cout<<"\n"<<horaTrabajada<<"\n";
+    
+    int horas = horaTrabajada/3600;
+    horaTrabajada %= 3600;
+    minutosTrabajados = horaTrabajada / 60;
+    segundosTrabajados %= 60;
+    
+    cout<<"tu trabajaste: "<<horas<<":"<<minutosTrabajados<<":"<<segundosTrabajados<<"\n";
+    
+    
+    
+    return 0;
+}
